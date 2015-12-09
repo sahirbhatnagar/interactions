8f9a2c8576cec2fa43e2b088ea6f736c4c5cb31c
sahirbhatnagar <sahir.bhatnagar@gmail.com>
2015-12-09 00:55:35 -0500

    commented the hierNet code. still need to figure out if its working correctly. it doesnt look like it is because the th matrix outputted from the hierNet function is not giving a symmetric matrix even though it should

--- code/hierNet.R
+++ code/hierNet.R
@@ -9,36 +9,52 @@
 ##################################
 
 rm(list = ls())
-source("packages.R")
+#source("packages.R")
+
+library(hierNet)
+library(magrittr)
+
 "%ni%" <- Negate("%in%")
 
 set.seed(12)
-p = 100
+
+# number of predictors
+p = 10
+
+# number of subjects
 n = 200
-x=matrix(rnorm(n*p),ncol=p)
-e=matrix(rbinom(n,1,0.5), ncol=1)
 
+# predictors
+x = matrix(rnorm(n*p),ncol=p)
 
+# environment
+e = matrix(rbinom(n,1,0.5), ncol=1)
 
+# create the zz matrix
 X <- hierNet::compute.interactions.c(cbind2(x,e)) 
-(columns.to.keep <- c(paste0(1:p, ":",1:p), X %>% colnames() %>% grep((p+1) %>% as.character(),., value = T)))
+
+# find columnames corresponding to main effects and interactions with 'e'
+(columns.to.keep <- c(paste0(1:p, ":",1:p), X %>% 
+                        colnames() %>% 
+                        grep((p+1) %>% as.character(),., value = T)))
+
+# set all other pairwise interactions to 0
 X[,colnames(X) %ni% columns.to.keep ] <- 0
 
 head(X)
 
-#X=scale(X[,columns.to.keep],TRUE,TRUE)
+# generate response
+y = X[,"1:1"]+ X[,"5:5"] + X[,"11:11"] + 1.5*X[,"1:11"] + 2*X[,"5:11"]+3*rnorm(n)
 
-y=X[,"1:1"]+ X[,"5:5"] + X[,"101:101"] + 1.5*X[,"1:101"] + 1.5*X[,"5:101"]+1.5*rnorm(n)
-fit=hierNet.path(cbind2(x,e),y, zz = X)
-fitcv=hierNet.cv(fit,cbind2(x,e),y)
+fit = hierNet.path(cbind2(x,e),y, zz = X)
+fitcv = hierNet.cv(fit,cbind2(x,e),y)
 print(fitcv)
 plot(fitcv)
 
-
-fitcv
-
+# matrix of estimated interaction coefficients 
 (d <- fit$th[,,which(fitcv$lamhat.1se==fit$lamlist)])
 
+# main effect estimates
 fit$bp[,which(fitcv$lamhat.1se==fit$lamlist), drop = F] - fit$bn[,which(fitcv$lamhat.1se==fit$lamlist), drop = F]
 
 
